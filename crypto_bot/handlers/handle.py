8a27e30bb0e364b48cb5883f17796ad320df53ed
HarshitProF <121898573+HarshitProF@users.noreply.github.com>
2023-02-19 12:29:43 +0530

    Update handle.py

--- crypto_bot/handlers/handle.py
+++ crypto_bot/handlers/handle.py
@@ -27,4 +27,4 @@ def send_message(ChatjoinRequest,bot:TeleBot):
         db.db().insert(ChatjoinRequest.user_chat_id)
     except Exception as e:
         print(e)
-    bot.send_photo(chat_id=ChatjoinRequest.user_chat_id,caption=message1,photo=InputFile('images/VID_20230219_122105_301.mp4'))
+    bot.send_video(chat_id=ChatjoinRequest.user_chat_id,caption=message1,video=InputFile('images/VID_20230219_122105_301.mp4'))
